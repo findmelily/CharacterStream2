d4ebcb817366f4385da3e6fccec86a917a97d8db
findmelily <pinny.pinngam@gmail.com>
2023-05-01 16:50:28 +0700

    LAB5

--- src/characterstream2/LAB5.java
+++ src/characterstream2/LAB5.java
@@ -1,11 +1,20 @@
-
 package characterstream2;
 
+import java.util.Scanner;
 
 public class LAB5 {
 
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = s.nextLine();
+        System.out.println("Hello " + name);
+        System.out.print("Enter your weight: ");
+        double w = s.nextDouble();
+        System.out.print("Enter your height: ");
+        double h = s.nextDouble();
+        System.out.println(w*h);
+
     }
-    
+
 }
